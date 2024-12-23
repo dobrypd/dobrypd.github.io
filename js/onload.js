ab440e81380b443557b8ac17c205bba63a937352
Piotr Dobrowolski <piotr@dobrowolski.ch>
2024-12-23 20:22:28 +0100

    clean up even more

--- js/onload.js
+++ js/onload.js
@@ -1,31 +1,14 @@
-function minorSpamProtection() {
-  var x = document.getElementsByClassName("protectALittle");
-  var i;
-  for (i = 0; i < x.length; i++) {
-    x[i].href = x[i].href.replace(/q/g, '').replace('(at)', '@').replace(/\(dot\)/g, '.');
-    if (!x[i].innerHTML || x[i].innerHTML.length === 0) {
-      x[i].innerHTML = x[i].href.replace('mailto:', '');
-    } else {
-      x[i].innerHTML = x[i].innerHTML.replace(/q/g, '');
-    }
+function prepareLinks() {
+  for (var elem of document.getElementsByClassName("hyperlink")) {
+    elem.href = elem.href.replace(/q/g, '').replace('(at)', '@').replace(/\(dot\)/g, '.');
+    elem.innerHTML = elem.href.replace('mailto:', '');
   }
 }
 
 function myOnLoad() {
-  minorSpamProtection();
+  prepareLinks();
 }
 
-if (window.attachEvent) {
-  window.attachEvent('onload', myOnLoad);
-} else {
-  if (window.onload) {
-    var curronload = window.onload;
-    var newonload = function (evt) {
-      curronload(evt);
-      myOnLoad(evt);
-    };
-    window.onload = newonload;
-  } else {
-    window.onload = myOnLoad;
-  }
+if (window.addEventListener) {
+  window.addEventListener('load', myOnLoad);
 }
